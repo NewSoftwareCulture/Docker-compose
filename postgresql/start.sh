d6efb125c4e6f50c11c09681c00d74ff81c259ba
Vlados <vlados.pochta@gmail.com>
2024-01-31 22:55:41 +0300

    fix postgres

--- postgresql/start.sh
+++ postgresql/start.sh
@@ -1,12 +1,13 @@
 # Create volume
-docker volume create postgres_vol_1
+# docker volume create postgres_vol_1
 
-# Create network
-docker network create app_sql_network
+# # Create network
+# docker network create app_sql_network
 
 # Run postgres container
 docker run --rm -d \
   --name postgres_1 \
+  -p 5432:5432 \
   -e POSTGRES_PASSWORD=admin \
   -e POSTGRES_USER=admin \
   -e POSTGRES_DB=app_sql \
